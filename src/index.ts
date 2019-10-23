98fe99b2af06082babe8708914a873847e65ff8d
wenye <1583247975@qq.com>
2019-10-23 15:22:36 +0800

    feat: add WenZhenKun obj

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 export function hello() {
   return "hello world!";
 }
+
+export const WenZhenKun = {
+  info: {
+    name: "温镇坤",
+    age: 23,
+    height: 170,
+    weight: "66kg",
+  },
+};
--- test/test-index.ts
+++ test/test-index.ts
@@ -1,9 +1,13 @@
-import { hello } from "../src/index";
+import * as hello from "../src/index";
 import assert from "assert";
 
 describe("test", function() {
-  it("index", function () {
-    const ret = hello();
+  it("hello", function() {
+    const ret = hello.hello();
     assert.equal(ret, "hello world!");
-  })
+  });
+  it("WenZhenKun", function() {
+    const ret = hello.WenZhenKun;
+    assert.ok(ret.info);
+  });
 });
